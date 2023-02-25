bc512fd330aa696d2304846f40500b713bd2841c
luke-obrien <lukeobrien02@gmail.com>
2023-02-25 16:21:20 +0000

    service update styling and refactoring

--- tflapp/src/pages/ServiceUpdates/ServiceUpdates.js
+++ tflapp/src/pages/ServiceUpdates/ServiceUpdates.js
@@ -5,29 +5,45 @@ import styles from "./ServiceUpdates.module.scss"
 
 function ServiceUpdates() {
 
+  const apiVariables = {
+    TFLLines: "tube,overground,dlr,tram,elizabeth-line",
+    River: "river-bus",
+    Bus: "bus"
+  }
   const [linestatus, setLineStatus] = useState(undefined)
-
+  const [mode, setMode ] = useState(apiVariables.TFLLines)
+  
   useEffect(() => {
     async function GetServiceData() {
-      const response = await fetch("https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tram,elizabeth-line/status/")
+      const response = await fetch(`https://api.tfl.gov.uk/line/mode/${mode}/status/`)
       const data = await response.json()
       console.log(data)
       setLineStatus(data)
     }
     GetServiceData()
-  }, []);
+  }, [mode]);
+
 
   return (
     <>
-    <h1>ServiceUpdates</h1>
+    <h1>Service updates</h1>
+    <div className={styles.modeContainer}>
+    <button className={ mode === apiVariables.TFLLines ? styles.modeButtonActive : styles.modeButton} onClick={(() => setMode(apiVariables.TFLLines))}>TFL Lines</button>
+    <button className={ mode === apiVariables.River ? styles.modeButtonActive : styles.modeButton} onClick={(() => setMode(apiVariables.River))}>River</button>
+    <button className={ mode === apiVariables.Bus ? styles.modeButtonActive : styles.modeButton} onClick={(() => setMode(apiVariables.Bus))}>Bus</button>
+    </div>
+    <div className={styles.linestatusContainer}>
     {linestatus && linestatus.map((line, i) => {
       return (
         <LineCard key={i} 
         line={line.name} 
         status={line.lineStatuses[0].statusSeverityDescription}
         description={line.lineStatuses[0].reason}
+        id={line.id}
+        statusSeverity={line.lineStatuses[0].statusSeverity}
         />
       )})}
+    </div>
     </>
   )
 }
--- tflapp/src/pages/ServiceUpdates/components/LineCard.js
+++ tflapp/src/pages/ServiceUpdates/components/LineCard.js
@@ -3,17 +3,21 @@ import { useState } from "react"
 
 import styles from "./LineCard.module.scss"
 
-function LineCard({line, status, description}) {
+function LineCard({line, status, description, id, statusSeverity}) {
 
   const [viewReason, setViewReason] = useState(false)
   
   return (
     <>
-    <div className={styles.LineCard}>
-      <h3>{line}</h3>
-      <p>{status}</p>
-      { description && <button onClick={(() => setViewReason(!viewReason))}>View details</button>}
-      { viewReason && <span>{description}</span>}
+    <div className={`${styles.LineCard} ${styles[id]}`}>
+      <div className={styles.cardContent}>
+        <div>
+          <h3 className={styles.lineName}>{line}</h3>
+          <p>{status}</p>
+        </div>
+        { description && <div className={styles.detailsContainer}><p className={styles.viewDetails} onClick={(() => setViewReason(!viewReason))}>View details</p></div>}
+      </div>
+      { viewReason && <p className={styles.descriptions}>{description}</p>}
     </div>
     </>
   )
